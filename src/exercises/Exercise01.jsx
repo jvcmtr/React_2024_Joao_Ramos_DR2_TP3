6b83fc6e470cb4b7e4d71dd88de309186e9ac474
jvcmtr <jvictorcmtr@gmail.com>
2024-09-14 17:56:24 -0300

    Exercise01

--- src/exercises/Exercise01.jsx
+++ src/exercises/Exercise01.jsx
@@ -1,13 +1,28 @@
-//Crie uma aplicação React onde você adicione estilos personalizados a um componente usando CSS externo. Crie um componente Card que possua classes para estilo e exiba uma informação básica, como nome e descrição.
+// Crie uma aplicação React onde você adicione estilos personalizados a um componente usando CSS externo.
+// Crie um componente Card que possua classes para estilo e exiba uma informação básica, como nome e descrição.
 // src/exercises/Exercise01.js
 import React from "react";
 
 const Exercise01 = () => {
   return (
     <div>
-      <h1>Exercise 01</h1>
+      lkdfjablfkjabçfjkbaçkfjnsçkjfnçkn
+      <Card/>
     </div>
   );
 };
+
+function Card(props){
+  return(
+    <div className="my_card">
+      <h2 className="my_card_title"> Title </h2>
+      <p className="my_card_description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Dignissimos molestiae quos quidem aspernatur repudiandae nam eligendi corrupti totam, 
+        soluta nostrum rerum magnam, voluptatum consequuntur ut laborum sunt accusantium provident optio!
+      </p>
+    </div>
+  )
+}
 
 export default Exercise01;
